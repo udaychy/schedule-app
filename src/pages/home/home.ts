1a1684461a8adcfcecb399cf382f066640caf8cb
udaychy <uday.choudhary.hsetin@gmail.com>
2018-04-30 20:57:45 +0530

    Add an ion button to display basic alert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController
+  ) {
 
   }
 
+  sayHello(){
+    let alert = this.alertCtrl.create({
+      title: 'Simple alert',
+      subTitle: 'Hello World!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  
 }
